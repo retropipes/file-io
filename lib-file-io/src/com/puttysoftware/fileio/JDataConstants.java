11c9db082b3508ebac0e88975d7a7104f6580c27
RetroPipes <eric.ahnell@puttysoftware.com>
2022-06-19 18:24:06 -0400

    Add JDataReader/Writer.

---
+++ lib-file-io/src/com/puttysoftware/fileio/JDataConstants.java
@@ -0,0 +1,14 @@
+/*  Diane Game Engine
+Copyleft (C) 2019 Eric Ahnell
+
+Any questions should be directed to the author via email at: support@puttysoftware.com
+ */
+package com.puttysoftware.fileio;
+
+class JDataConstants {
+    private JDataConstants() {
+    }
+
+    public static final String J_HEADER = "{"; //$NON-NLS-1$
+    public static final String J_FOOTER = "}"; //$NON-NLS-1$
+}
--- lib-file-io/src/com/puttysoftware/fileio/JDataReader.java
+++ lib-file-io/src/com/puttysoftware/fileio/JDataReader.java
@@ -12,41 +12,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
 public class JDataReader implements DataIOReader {
     // Fields
     private final BufferedReader fileIO;
     private final File file;
-    private final String docTag;
 
     // Constructors
-    public JDataReader(final String filename, final String newDocTag) throws IOException {
+    public JDataReader(final String filename) throws IOException {
 	this.fileIO = new BufferedReader(new FileReader(filename));
 	this.file = new File(filename);
-	this.docTag = newDocTag;
-	this.readXHeader();
-	this.readOpeningDocTag();
+	this.readJHeader();
     }
 
-    public JDataReader(final File filename, final String newDocTag) throws IOException {
+    public JDataReader(final File filename) throws IOException {
 	this.fileIO = new BufferedReader(new FileReader(filename));
 	this.file = filename;
-	this.docTag = newDocTag;
-	this.readXHeader();
-	this.readOpeningDocTag();
+	this.readJHeader();
     }
 
-    public JDataReader(final InputStream stream, final String newDocTag) throws IOException {
+    public JDataReader(final InputStream stream) throws IOException {
 	this.fileIO = new BufferedReader(new InputStreamReader(stream));
 	this.file = null;
-	this.docTag = newDocTag;
-	this.readXHeader();
-	this.readOpeningDocTag();
+	this.readJHeader();
     }
 
     // Methods
     @Override
     public DataMode getDataIOMode() {
-	return DataMode.CUSTOM_XML;
+	return DataMode.JSON;
     }
 
     @Override
@@ -57,7 +53,7 @@ public class JDataReader implements DataIOReader {
     @Override
     public void close() throws DataIOException {
 	try {
-	    this.readClosingDocTag();
+	    this.readJFooter();
 	    this.fileIO.close();
 	} catch (IOException e) {
 	    throw new DataIOException(e);
@@ -69,14 +65,12 @@ public class JDataReader implements DataIOReader {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
-	} catch (IOException e) {
+	    if (line != null) {
+		final String strobj = Jsoner.deserialize(line).toString();
+		return Double.parseDouble(strobj);
+	    }
+	} catch (IOException | JsonException e) {
 	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    final String[] split = JDataReader.splitLine(line);
-	    JDataReader.validateOpeningTag(split[0], XDataConstants.DOUBLE_TAG);
-	    JDataReader.validateClosingTag(split[2], XDataConstants.DOUBLE_TAG);
-	    return Double.parseDouble(split[1]);
 	}
 	throw new DataIOException("End of file!"); //$NON-NLS-1$
     }
@@ -86,14 +80,12 @@ public class JDataReader implements DataIOReader {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
-	} catch (IOException e) {
+	    if (line != null) {
+		final String strobj = Jsoner.deserialize(line).toString();
+		return Integer.parseInt(strobj);
+	    }
+	} catch (IOException | JsonException e) {
 	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    final String[] split = JDataReader.splitLine(line);
-	    JDataReader.validateOpeningTag(split[0], XDataConstants.INT_TAG);
-	    JDataReader.validateClosingTag(split[2], XDataConstants.INT_TAG);
-	    return Integer.parseInt(split[1]);
 	}
 	throw new DataIOException("End of file!"); //$NON-NLS-1$
     }
@@ -103,14 +95,12 @@ public class JDataReader implements DataIOReader {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
-	} catch (IOException e) {
+	    if (line != null) {
+		final String strobj = Jsoner.deserialize(line).toString();
+		return Long.parseLong(strobj);
+	    }
+	} catch (IOException | JsonException e) {
 	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    final String[] split = JDataReader.splitLine(line);
-	    JDataReader.validateOpeningTag(split[0], XDataConstants.LONG_TAG);
-	    JDataReader.validateClosingTag(split[2], XDataConstants.LONG_TAG);
-	    return Long.parseLong(split[1]);
 	}
 	throw new DataIOException("End of file!"); //$NON-NLS-1$
     }
@@ -120,14 +110,12 @@ public class JDataReader implements DataIOReader {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
-	} catch (IOException e) {
+	    if (line != null) {
+		final String strobj = Jsoner.deserialize(line).toString();
+		return Byte.parseByte(strobj);
+	    }
+	} catch (IOException | JsonException e) {
 	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    final String[] split = JDataReader.splitLine(line);
-	    JDataReader.validateOpeningTag(split[0], XDataConstants.BYTE_TAG);
-	    JDataReader.validateClosingTag(split[2], XDataConstants.BYTE_TAG);
-	    return Byte.parseByte(split[1]);
 	}
 	throw new DataIOException("End of file!"); //$NON-NLS-1$
     }
@@ -150,14 +138,12 @@ public class JDataReader implements DataIOReader {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
-	} catch (IOException e) {
+	    if (line != null) {
+		final String strobj = Jsoner.deserialize(line).toString();
+		return Boolean.parseBoolean(strobj);
+	    }
+	} catch (IOException | JsonException e) {
 	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    final String[] split = JDataReader.splitLine(line);
-	    JDataReader.validateOpeningTag(split[0], XDataConstants.BOOLEAN_TAG);
-	    JDataReader.validateClosingTag(split[2], XDataConstants.BOOLEAN_TAG);
-	    return Boolean.parseBoolean(split[1]);
 	}
 	throw new DataIOException("End of file!"); //$NON-NLS-1$
     }
@@ -167,14 +153,12 @@ public class JDataReader implements DataIOReader {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
-	} catch (IOException e) {
+	    if (line != null) {
+		final String strobj = Jsoner.deserialize(line).toString();
+		return strobj;
+	    }
+	} catch (IOException | JsonException e) {
 	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    final String[] split = JDataReader.splitLine(line);
-	    JDataReader.validateOpeningTag(split[0], XDataConstants.STRING_TAG);
-	    JDataReader.validateClosingTag(split[2], XDataConstants.STRING_TAG);
-	    return JDataReader.replaceSpecialCharacters(split[1]);
 	}
 	throw new DataIOException("End of file!"); //$NON-NLS-1$
     }
@@ -221,63 +205,7 @@ public class JDataReader implements DataIOReader {
 	return line == null;
     }
 
-    public void readOpeningGroup(final String groupName) throws DataIOException {
-	String line = "";
-	try {
-	    line = this.fileIO.readLine();
-	} catch (IOException e) {
-	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    JDataReader.validateOpeningTag(JDataReader.replaceSpecialCharacters(line), groupName);
-	} else {
-	    throw new DataIOException("End of file!");
-	}
-    }
-
-    public void readClosingGroup(final String groupName) throws DataIOException {
-	String line = "";
-	try {
-	    line = this.fileIO.readLine();
-	} catch (IOException e) {
-	    throw new DataIOException(e);
-	}
-	if (line != null) {
-	    JDataReader.validateClosingTag(JDataReader.replaceSpecialCharacters(line), groupName);
-	} else {
-	    throw new DataIOException("End of file!");
-	}
-    }
-
-    private static void validateOpeningTag(final String tag, final String tagType) throws DataIOException {
-	if (!tag.equals("<" + tagType + ">")) { //$NON-NLS-1$ //$NON-NLS-2$
-	    throw new DataIOException("Expected opening tag of <" //$NON-NLS-1$
-		    + tagType + ">, found " + tag + "!"); //$NON-NLS-1$ //$NON-NLS-2$
-	}
-    }
-
-    private static void validateClosingTag(final String tag, final String tagType) throws DataIOException {
-	if (!tag.equals("</" + tagType + ">")) { //$NON-NLS-1$ //$NON-NLS-2$
-	    throw new DataIOException("Expected closing tag of </" //$NON-NLS-1$
-		    + tagType + ">, found " + tag + "!"); //$NON-NLS-1$ //$NON-NLS-2$
-	}
-    }
-
-    private static String[] splitLine(final String line) throws DataIOException {
-	final String[] split = new String[3];
-	final int loc0 = line.indexOf('>') + 1;
-	final int loc2 = line.indexOf('<', loc0);
-	if (loc0 == -1 || loc2 == -1) {
-	    throw new DataIOException("Unexpected string found: " //$NON-NLS-1$
-		    + line + "!"); //$NON-NLS-1$
-	}
-	split[0] = line.substring(0, loc0);
-	split[1] = line.substring(loc0, loc2);
-	split[2] = line.substring(loc2);
-	return split;
-    }
-
-    private void readXHeader() throws DataIOException {
+    private void readJHeader() throws DataIOException {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
@@ -287,44 +215,23 @@ public class JDataReader implements DataIOReader {
 	if (line == null) {
 	    throw new DataIOException("Corrupt or invalid header!"); //$NON-NLS-1$
 	}
-	if (!line.equals(XDataConstants.X_HEADER)) {
+	if (!line.equals(JDataConstants.J_HEADER)) {
 	    throw new DataIOException("Corrupt or invalid header!"); //$NON-NLS-1$
 	}
     }
 
-    private void readOpeningDocTag() throws DataIOException {
+    private void readJFooter() throws DataIOException {
 	String line = "";
 	try {
 	    line = this.fileIO.readLine();
 	} catch (IOException e) {
 	    throw new DataIOException(e);
 	}
-	if (line != null && !line.equals("<" + this.docTag + ">")) { //$NON-NLS-1$ //$NON-NLS-2$
-	    throw new DataIOException("Opening doc tag does not match: expected <" + this.docTag //$NON-NLS-1$
-		    + ">, found " + line + "!"); //$NON-NLS-1$ //$NON-NLS-2$
+	if (line == null) {
+	    throw new DataIOException("Corrupt or invalid footer!"); //$NON-NLS-1$
 	}
-    }
-
-    private void readClosingDocTag() throws DataIOException {
-	String line = "";
-	try {
-	    line = this.fileIO.readLine();
-	} catch (IOException e) {
-	    throw new DataIOException(e);
+	if (!line.equals(JDataConstants.J_FOOTER)) {
+	    throw new DataIOException("Corrupt or invalid footer!"); //$NON-NLS-1$
 	}
-	if (line != null && !line.equals("</" + this.docTag + ">")) { //$NON-NLS-1$ //$NON-NLS-2$
-	    throw new DataIOException("Closing doc tag does not match: expected </" + this.docTag //$NON-NLS-1$
-		    + ">, found " + line + "!"); //$NON-NLS-1$ //$NON-NLS-2$
-	}
-    }
-
-    private static String replaceSpecialCharacters(final String s) {
-	String r = s;
-	r = r.replace("&amp;", "&"); //$NON-NLS-1$ //$NON-NLS-2$
-	r = r.replace("&lt;", "<"); //$NON-NLS-1$ //$NON-NLS-2$
-	r = r.replace("&gt;", ">"); //$NON-NLS-1$ //$NON-NLS-2$
-	r = r.replace("&quot;", "\""); //$NON-NLS-1$ //$NON-NLS-2$
-	r = r.replace("&apos;", "\'"); //$NON-NLS-1$ //$NON-NLS-2$
-	return r.replace("&#xA;", "\n"); //$NON-NLS-1$ //$NON-NLS-2$
     }
 }
